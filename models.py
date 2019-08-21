9262655de27daf336aa774a85323bb753750989f
Sergey Goncharov <gsa977@gmail.com>
2019-08-22 02:34:27 +0300

    fixes for models

--- models.py
+++ models.py
@@ -10,23 +10,25 @@ class MyEnum(db.Enum):
     two = 'female'
 
 
-relatives = db.Table('relatives',
-                     db.Column('request_id', db.String, db.ForeignKey('citizens.request_id')),
-                     db.Column('first_id', db.Integer, db.ForeignKey('citizens.citizen_id')),
-                     db.Column('second_id', db.Integer, db.ForeignKey('citizens.citizen_id'))
-                     )
+class Relatives(db.Model):
+    __tablename__ = 'relatives'
+
+    request_id = db.Column(db.Integer, db.ForeignKey('requests.id'))
+    first_id = db.Column(db.Integer, db.ForeignKey('citizens.id'))
+    second_id = db.Column(db.Integer, db.ForeignKey('citizens.id'))
 
 
 class Request(db.Model):
     __tablename__ = 'requests'
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.JSONB())
+    data = db.Column(db.String())
 
 
 class Citizen(db.Model):
     __tablename__ = 'citizens'
 
-    request_id = db.ForeignKey('requests.id')
+    request_id = db.Column(db.Integer, db.ForeignKey('requests.id'))
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
     citizen_id = db.Column(db.Integer, nullable=False)
     town = db.Column(db.String(256), nullable=False)
     street = db.Column(db.String(256), nullable=False)
@@ -34,11 +36,9 @@ class Citizen(db.Model):
     apartment = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(256), nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
-    gender = db.Column(db.Enum())
+    gender = db.Column(MyEnum(name='gender'))
 
-    relatives: db.relationship("Citizen", secondary=relatives)
-
-    _pk = db.PrimaryKeyConstraint('request_id', 'citizen_id', name='requestAndCitizen')
+    _pk = db.Index('requestAndCitizen', 'request_id', 'citizen_id', unique=True)
 
 
 async def main():
